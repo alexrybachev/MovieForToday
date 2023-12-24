19c5f9a7ec5870423f42008294f46803fb92011f
Ilia Shapovalov <greateman@mail.ru>
2023-12-24 20:31:03 +0300

    Add movie list endpoint. Add tests.

---
+++ MyLibrary/Sources/Networking/Endpoint/Endpoint.swift
@@ -0,0 +1,81 @@
+//
+//  Endpoint.swift
+//
+//
+//  Created by Илья Шаповалов on 24.12.2023.
+//
+
+import Foundation
+
+struct Endpoint {
+    let path: String
+    let queryItems: [URLQueryItem]
+    let method: HTTPMethod
+    
+    var url: URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.kinopoisk.dev"
+        components.path = "/v1.4/".appending(path)
+        
+        if !queryItems.isEmpty {
+            components.queryItems = queryItems
+        }
+        guard let url = components.url else {
+            fatalError("Unable to create url from components: \(components)")
+        }
+        return url
+    }
+    
+    //MARK: - init(_:)
+    private init(
+        method: HTTPMethod,
+        path: String,
+        @QueryBuilder queryItems: () -> [URLQueryItem]
+    ) {
+        self.method = method
+        self.path = path
+        self.queryItems = queryItems()
+    }
+    
+    //MARK: - URLRequest builder
+    @inlinable
+    @inline(__always)
+    func request(_ payload: Data? = nil) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        return request
+    }
+    
+    //MARK: - Constructers
+    @inlinable
+    @inline(__always)
+    static func movieList(page: Int = 1, limit: Int = 10) -> Self {
+        .init(
+            method: .GET,
+            path: "list"
+        ) {
+            paginated(page: page, limit: limit)
+        }
+    }
+}
+
+//MARK: - Helpers
+extension Endpoint {
+    @inlinable
+    @inline(__always)
+    @QueryBuilder
+    static func paginated(page: Int, limit: Int) -> [URLQueryItem] {
+        URLQueryItem(name: "page", value: page.description)
+        URLQueryItem(name: "limit", value: limit.description)
+    }
+}
+
+extension Endpoint {
+    enum HTTPMethod: String {
+        case GET
+        case PUT
+        case POST
+        case DELETE
+    }
+}
--- MyLibrary/Tests/NetworkingTests/EndpointTests.swift
+++ MyLibrary/Tests/NetworkingTests/EndpointTests.swift
@@ -9,5 +9,21 @@ import XCTest
 @testable import Networking
 
 final class EndpointTests: XCTestCase {
-
+    func test_movieList() {
+        let sut = Endpoint.movieList(page: 1, limit: 10)
+        
+        XCTAssertEqual(
+            sut.url,
+            URL(string: "https://api.kinopoisk.dev/v1.4/list?page=1&limit=10")
+        )
+    }
+    
+    func test_movieListPaginated() {
+        let sut = Endpoint.movieList(page: 2, limit: 1)
+        
+        XCTAssertEqual(
+            sut.url,
+            URL(string: "https://api.kinopoisk.dev/v1.4/list?page=2&limit=1")
+        )
+    }
 }
