67a1ee70ee57266544bc0f0850209222fe8a9ef1
Alex Rybachev <rybachev.as@gmail.com>
2023-12-24 16:23:11 +0400

    add methods for fonts and colors

---
+++ MovieForToday/Helpers/Font+ext.swift
@@ -0,0 +1,20 @@
+//
+//  Font+ext.swift
+//  MovieForToday
+//
+//  Created by Aleksandr Rybachev on 24.12.2023.
+//
+
+import SwiftUI
+
+enum CustomFont: String {
+    case montMedium = "Montserrat-Medium"
+    case montRegular = "Montserrat-Regular"
+    case montSemiBold = "Montserrat-SemiBold"
+}
+
+extension Font {
+    static func custom(_ nameFont: CustomFont, size: CGFloat) -> SwiftUI.Font {
+        SwiftUI.Font.custom(nameFont.rawValue, size: size)
+    }
+}
