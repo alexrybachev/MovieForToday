0646ced675644e9ab386f931c1883541277729e1
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-12-27 18:07:45 +0300

    added boilerplate code

---
+++ MovieForToday/MovieDetailScreen/MovieDetailNavBar.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailNavBar.swift
+//  MovieForToday
+//
+//  Created by Андрей Бородкин on 27.12.2023.
+//
+
+import SwiftUI
+
+struct MovieDetailNavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailNavBar()
+}
